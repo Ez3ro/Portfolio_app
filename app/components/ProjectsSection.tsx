2b88a494c26f4ea0bb724280fb9c0b40ee02fc6e
ezekhielparas <ezekhiel.paras@webit.com.au>
2025-02-03 21:38:04 +0800

    images2

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -15,7 +15,7 @@ interface ProjectsSectionProps {
 const ProjectsSection: React.FC<ProjectsSectionProps> = ({ projects }) => {
   return (
     <section className="text-center">
-      <h2 className="text-2xl font-semibold">Projects</h2>
+      <h2 className="text-2xl font-semibold"><span className='text-green-500'>Some</span> of my Projects</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
         {projects.map((project) => (
           <ProjectCard
